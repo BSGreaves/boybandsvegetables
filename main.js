4432a30e64f4a11e1f3602a20c726f6a3ee4d1b8
Ben Greaves <bsgreaves@gmail.com>
2017-01-25 20:42:14 -0600

    Assignment complete

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-Placeholder
+// Loop through the two arrays provided (bands and vegetables) and output each element in the arrays into their corresponding HTML <div> element. Ensure that each item is in a block element (e.g. li, div, p. etc...)
+
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+var bandElement = document.getElementById("boy-bands");
+var veggieElement = document.getElementById("vegetables");
+
+for (var i = 0; i < bands.length; i++) {
+	bandElement.innerHTML += "<div>" + bands[i] + "</div>" + "<br>";
+	veggieElement.innerHTML += "<div>" + vegetables[i] + "</div>" + "<br>";
+}
+
+
+
+
+
